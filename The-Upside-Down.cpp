c7a3b0c69782cd4aca1c6f98db614a9562c76531
Dyego Anjos <107328787+DyegoAnjos@users.noreply.github.com>
2022-09-29 16:31:27 -0300

    29/09

--- The-Upside-Down.cpp
+++ The-Upside-Down.cpp
@@ -9,52 +9,113 @@ int linha(){
 }
 
 int main(){
+	
 	struct tpessoa{
 		char nome[50];
-		int posicao[1], posicaoEspelho;	
+		int posicao[1], posicaoEspelho,mundo=-1;
 	}; struct tpessoa pessoa[50];
 	
-	int i=0;
+	int i=0, menu=0;
+	int resultado=0;
 	char escolha;
 	FILE *arq;
 	
-	arq = fopen("Histórico.txt", "a+");
-	setlocale(LC_ALL, "Portuguese");
-	printf("::::::::::Localizador do Mundo Invertido::::::::::");
-	for(i=0;i<50;i++){
-		printf("\nDigite o nome da pessoa:");
-		scanf("%s", &pessoa[i].nome);
-		
-		printf("\nDigite a posição X:");
-		scanf("%d", &pessoa[i].posicao[0]);
-		
-		printf("\nDigite a posição Y:");
-		scanf("%d", &pessoa[i].posicao[1]);
-		
-		printf("\nDeseja continuar? S/N");
-		escolha = toupper(getche());
-		if (escolha == 'N') break;
-	}
+	while(1){
 	system("cls");
+	setlocale(LC_ALL, "Portuguese");
+	arq = fopen("Histórico.txt", "a+");
 	
-	for(int resultado=0;resultado<=i;resultado++){
-		printf("\nPessoa:%s", pessoa[resultado].nome);
-		printf("\nPosição X:%d", pessoa[resultado].posicao[0]);
-		printf("\nPosição Y:%d", pessoa[resultado].posicao[1]);
-		if (pessoa[resultado].posicao[1]<0)
-			printf("\n--Mundo Invertido--");
-		else if (pessoa[resultado].posicao[1]==0)
-			printf("Ponto de interceção (Está em ambas as partes)");
-			
-		pessoa[resultado].posicaoEspelho=pessoa[resultado].posicao[1]*-1;
-		printf("\n::::::::::Ponto Espelhado::::::::::\n");
-		printf("Posição X:%d", pessoa[resultado].posicao[0]);
-		printf("\nPosição Y:%d", pessoa[resultado].posicaoEspelho);
-		linha();
-		fprintf(arq, "\nNome:%s\nPosição X:%d\nPosição Y:%d\nPosição Y espelhada:%d\n", pessoa[resultado].nome, pessoa[resultado].posicao[0], pessoa[resultado].posicao[1], pessoa[resultado].posicaoEspelho);
+	printf("::::::::::Localizador do Mundo Invertido::::::::::");
+	printf("\nLocalizar(1)\nLista dos cadastros (2)\nVer histórico(3)\nImprimir no cartesiano(4)\nSair(0)\n");
+	scanf("%d", &menu);
+	
+	switch (menu){
+		case 0:{
+			return(0);
+		break;}
+		case 1:{
+			for(i=0;i<50;i++){
+				printf("\nDigite o nome da pessoa:");
+				scanf("%s", &pessoa[i].nome);
+				
+				printf("\nDigite a posição X:");
+				scanf("%d", &pessoa[i].posicao[0]);
+				
+				printf("\nDigite a posição Y:");
+				scanf("%d", &pessoa[i].posicao[1]);
+				
+				if (pessoa[i].posicao[1]<0)
+					pessoa[i].mundo=-1;
+				
+				else if (pessoa[i].posicao[1]==0)
+					pessoa[i].mundo=0;
+				
+				else
+					pessoa[i].mundo=1;
+					
+				pessoa[i].posicaoEspelho=pessoa[i].posicao[1];
+				pessoa[i].posicaoEspelho*=-1;
+					
+				printf("\nDeseja continuar? S/N");
+				escolha = toupper(getche());
+				if (escolha == 'N') break;
+			}
+		break;}
+		
+		case 2:{
+			for(resultado=i;resultado>=i;resultado--){
+				printf("\nPessoa:%s", pessoa[resultado].nome);
+				printf("\nPosição X:%d", pessoa[resultado].posicao[0]);
+				printf("\nPosição Y:%d", pessoa[resultado].posicao[1]);
+				if (pessoa[resultado].mundo == -1)
+					printf("\n--Mundo Invertido--");
+				
+				else if (pessoa[resultado].mundo == 0)
+					printf("\n--Ponto de interceção (Está em ambas as partes)--");
+				
+				else
+					printf("\n--Mundo Normal--");
+				
+				printf("\n::::::::::Ponto Espelhado::::::::::\n");
+				printf("Posição X:%d", pessoa[resultado].posicao[0]);
+				printf("\nPosição Y:%d", pessoa[resultado].posicaoEspelho);
+				linha();
+				system("pause");
+			}
+		break;}
+		
+		case 3:{
+			arq = fopen("Histórico.txt", "r");
+			while(1){
+				i++;
+				fread(&pessoa,sizeof(struct tpessoa),1,arq);
+				printf("\nPessoa:%s", pessoa[i].nome);
+				printf("\nPosição X:%d", pessoa[i].posicao[0]);
+				printf("\nPosição Y:%d", pessoa[i].posicao[1]);
+				if (pessoa[i].posicao[1]<0){
+					printf("\n--Mundo Invertido--");
+				}
+				
+				else if (pessoa[i].posicao[1]==0){
+					printf("\n--Ponto de interceção (Está em ambas as partes)--");
+				}
+				
+				else{
+					printf("\n--Mundo Normal--");
+					
+				}				
+				printf("\n::::::::::Ponto Espelhado::::::::::\n");
+				printf("Posição X:%d", pessoa[i].posicao[0]);
+				printf("\nPosição Y:%d", pessoa[i].posicaoEspelho);
+				system("pause");
+			}
+		break;}
+	}
+	
 	}
 	fclose(arq);
 		
 	getch();
-	return(0);
 }
+
+
